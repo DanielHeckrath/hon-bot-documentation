aa78ee3532a16f698156ac64e47731b0464436da
Daniel Heckrath <dh@milosys.de>
2013-02-13 12:31:10 +0100

    use @treturn instead of @tparam

--- lua/CTeamBotBrain.lua
+++ lua/CTeamBotBrain.lua
@@ -3,14 +3,14 @@
 
 --- Function to check if the target unit can be seen
 -- @param unitTarget the target unit
--- @tparam bool UnitCanBeSeen wether the target can be seen or not
+-- @treturn bool wether the target can be seen or not
 function CTeamBotBrain:CanSeeUnit(unitTarget)
 
 --- Function to get the position of the front of the creepwave
 -- @tparam string sLane the lane
--- @tparam Vector3 Position the position of the front of the creepwave
+-- @treturn Vector3 the position of the front of the creepwave
 function CTeamBotBrain:GetFrontOfCreepWavePosition(sLane)
 
 --- Function to get the team of the bot
--- @return number the team
+-- @treturn number the team
 function CTeamBotBrain:GetTeam()
