d58875278599fd665a01902d8042ad9ba647a696
Daniel Heckrath <dh@milosys.de>
2013-02-13 12:26:51 +0100

    Update to typed params to include a name for the value

--- lua/CTeamBotBrain.lua
+++ lua/CTeamBotBrain.lua
@@ -3,12 +3,12 @@
 
 --- Function to check if the target unit can be seen
 -- @param unitTarget the target unit
--- @tparam bool wether the target can be seen or not
+-- @tparam bool UnitCanBeSeen wether the target can be seen or not
 function CTeamBotBrain:CanSeeUnit(unitTarget)
 
 --- Function to get the position of the front of the creepwave
 -- @tparam string sLane the lane
--- @tparam Vector3 the position of the front of the creepwave
+-- @tparam Vector3 Position the position of the front of the creepwave
 function CTeamBotBrain:GetFrontOfCreepWavePosition(sLane)
 
 --- Function to get the team of the bot
